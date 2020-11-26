a3737574f1f7d7c1ba77e7c2db07aa7a3d862a16
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-26 19:50:21 +0900

    NoComplete Java (Error) :(

--- app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/MainActivity.java
@@ -2,7 +2,13 @@ package kr.hs.emirim.s2019w04.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editInput1 = (EditText) findViewById(R.id.input_1);
+        final EditText editInput2 = (EditText) findViewById(R.id.input_2);
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnCal);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                switch (radioGroup.getCheckedRadioButtonId()){
+                    case R.id.radioadd:
+                        intent.putExtra("cal", "+");
+                        break;
+                    case R.id.radiominus:
+                        intent.putExtra("cal", "-");
+                        break;
+                    case R.id.radiomul:
+                        intent.putExtra("cal", "*");
+                        break;
+                    case R.id.radiodiv:
+                        intent.putExtra("cal", "/");
+                        break;
+                    default:
+                        break;
+                }
+                intent.putExtra("num1", Integer.parseInt(editInput1.getText().toString()));
+                intent.putExtra("num2", Integer.parseInt(editInput2.getText().toString()));
+
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            int hap = data.getIntExtra("Hap", 0);
+            Toast.makeText(getApplicationContext(), "결과" +hap, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/ResultActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.s2019w04.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent iintent = getIntent();
+        String calc = (iintent.getStringExtra("Clac"));
+
+        int calValue = 0;
+        if(calc.equals("+")){
+            calValue = iintent.getIntExtra("num1", 0) + iintent.getIntExtra("num2", 0);
+        } else if (calc.equals("-")){
+            calValue = iintent.getIntExtra("num1", 0) - iintent.getIntExtra("num2", 0);
+        }else if(calc.equals("*")){
+            calValue = iintent.getIntExtra("num1", 0) * iintent.getIntExtra("num2", 0);
+        }else{
+            calValue = iintent.getIntExtra("num1", 0) / iintent.getIntExtra("num2", 0);
+        }
+        final int reValue = calValue;
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent outIntent = new Intent(getApplicationContext(), MainActivity.class);
+                outIntent.putExtra("Hap", reValue);
+                setResult(RESULT_OK, outIntent);
+                finish();
+            }
+        });
     }
 }
