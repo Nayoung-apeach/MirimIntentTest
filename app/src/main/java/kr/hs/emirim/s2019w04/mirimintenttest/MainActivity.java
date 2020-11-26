c6ef583ebf3e09a9d572e464120c2a239cef6339
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-26 19:52:23 +0900

    Complete java :D

--- app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/MainActivity.java
@@ -2,58 +2,70 @@ package kr.hs.emirim.s2019w04.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1,edit2;
+    Button btnAdd,btnMin,btnMul,btnDiv;
+    String num1,num2;
+    TextView res;
+    Integer textres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final EditText editInput1 = (EditText) findViewById(R.id.input_1);
-        final EditText editInput2 = (EditText) findViewById(R.id.input_2);
-        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
-
-        Button btnNewActivity = (Button) findViewById(R.id.btnCal);
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+        edit1=(EditText)findViewById(R.id.edit1);
+        edit2=(EditText)findViewById(R.id.edit2);
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+        btnMin=(Button)findViewById(R.id.btnMin);
+        btnMul=(Button)findViewById(R.id.btnMul);
+        btnDiv=(Button)findViewById(R.id.btnDiv);
+        res=(TextView)findViewById(R.id.TextRes);
+        View.OnClickListener Listener=new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                switch (radioGroup.getCheckedRadioButtonId()){
-                    case R.id.radioadd:
-                        intent.putExtra("cal", "+");
+            public void onClick(View view) {
+                switch(view.getId())
+                {
+                    case R.id.btnAdd:
+                        num1=edit1.getText().toString();
+                        num2=edit2.getText().toString();
+                        textres=Integer.parseInt(num1)+Integer.parseInt(num2);
+                        res.setText("계산결과:"+textres);
                         break;
-                    case R.id.radiominus:
-                        intent.putExtra("cal", "-");
+                    case R.id.btnMin:
+                        num1=edit1.getText().toString();
+                        num2=edit2.getText().toString();
+                        textres=Integer.parseInt(num1)-Integer.parseInt(num2);
+                        res.setText("계산결과:"+textres);
                         break;
-                    case R.id.radiomul:
-                        intent.putExtra("cal", "*");
+                    case R.id.btnMul:
+
+
+
+                        num1=edit1.getText().toString();
+                        num2=edit2.getText().toString();
+                        textres=Integer.parseInt(num1)*Integer.parseInt(num2);
+                        res.setText("계산결과:"+textres);
                         break;
-                    case R.id.radiodiv:
-                        intent.putExtra("cal", "/");
-                        break;
-                    default:
+                    case R.id.btnDiv:
+                        num1=edit1.getText().toString();
+                        num2=edit2.getText().toString();
+                        textres=Integer.parseInt(num1)/Integer.parseInt(num2);
+                        res.setText("계산결과:"+textres);
                         break;
                 }
-                intent.putExtra("num1", Integer.parseInt(editInput1.getText().toString()));
-                intent.putExtra("num2", Integer.parseInt(editInput2.getText().toString()));
-
-                startActivityForResult(intent, 0);
             }
-        });
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(resultCode == RESULT_OK){
-            int hap = data.getIntExtra("Hap", 0);
-            Toast.makeText(getApplicationContext(), "결과" +hap, Toast.LENGTH_SHORT).show();
-        }
+        };
+        btnAdd.setOnClickListener(Listener);
+        btnMin.setOnClickListener(Listener);
+        btnMul.setOnClickListener(Listener);
+        btnDiv.setOnClickListener(Listener);
     }
 }
