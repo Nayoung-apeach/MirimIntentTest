299a01a9dfc5e91cd82950d2d7fe0789c5549a5b
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-26 20:01:56 +0900

    Complete Resultjava :D

--- app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimintenttest/ResultActivity.java
@@ -1,43 +1,16 @@
 package kr.hs.emirim.s2019w04.mirimintenttest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-public class ResultActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-
-        Intent iintent = getIntent();
-        String calc = (iintent.getStringExtra("Clac"));
-
-        int calValue = 0;
-        if(calc.equals("+")){
-            calValue = iintent.getIntExtra("num1", 0) + iintent.getIntExtra("num2", 0);
-        } else if (calc.equals("-")){
-            calValue = iintent.getIntExtra("num1", 0) - iintent.getIntExtra("num2", 0);
-        }else if(calc.equals("*")){
-            calValue = iintent.getIntExtra("num1", 0) * iintent.getIntExtra("num2", 0);
-        }else{
-            calValue = iintent.getIntExtra("num1", 0) / iintent.getIntExtra("num2", 0);
-        }
-        final int reValue = calValue;
-
-        Button btnReturn = (Button) findViewById(R.id.btnReturn);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent outIntent = new Intent(getApplicationContext(), MainActivity.class);
-                outIntent.putExtra("Hap", reValue);
-                setResult(RESULT_OK, outIntent);
-                finish();
-            }
-        });
-    }
-}
+public class ResultActivity extends Activity
+{ @Override protected void onCreate(Bundle savedInstanceState)
+{ super.onCreate(savedInstanceState); setContentView(R.layout.activity_result);
+setTitle("Second 액티비티"); Intent inIntent = getIntent();
+final int hapValue = inIntent.getIntExtra("Num1", 0) + inIntent.getIntExtra("Num2", 0);
+Button btnReturn = (Button) findViewById(R.id.btnReturn);
+btnReturn.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View view)
+{ Intent outIntent = new Intent(getApplicationContext(), MainActivity.class); outIntent.putExtra("Hap", hapValue); setResult(RESULT_OK, outIntent); finish();
+} }); } }
